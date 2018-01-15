1fe227040c267aee80bbaaac255ff671b9288c5e
huskyhoochu <dfg1499@gmail.com>
2018-01-15 19:39:11 +0900

    practice update

--- app.js
+++ app.js
@@ -8,3 +8,49 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
+
+var scores, roundScore, activePlayer, gamePlaying;
+
+function init() {
+    scores = [0, 0];
+    roundScore = 0;
+    activePlayer = 0;
+    gamePlaying = true;
+
+// 모든 점수를 초기
+    document.getElementById('score-0').textContent = '0';
+    document.getElementById('score-1').textContent = '0';
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+}
+
+init();
+
+document.querySelector('.btn-roll').addEventListener('click', function () {
+    if (gamePlaying) {
+
+        // 1. Random number
+        var dice = Math.floor(Math.random() * 6) + 1;
+
+        // 2. Display the result
+        var diceDOM = document.querySelector('.dice');
+        diceDOM.style.display = 'block';
+        diceDOM.src = 'dice-' + dice + '.png';
+        document.querySelector('#current-' + activePlayer).textContent = dice.toString();
+
+        // 3. Update the round score if the rolled number was not a 1
+
+        if (dice !== 1) {
+            roundScore += dice;
+            document.querySelector('#score-' + activePlayer).textContent = roundScore;
+        } else {
+            // 삼항 연산자
+            activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+            document.getElementById('current-0').textContent = '0';
+            document.getElementById('current-1').textContent = '0';
+        }
+    }
+});
+
+document.querySelector('.btn-new').addEventListener('click', init);
+
